2533a7c66960ca9b5a69d7c2485f36ac1b9fb82f
ultras-koniuzez <273780@student.pwr.edu.pl>
2025-12-08 12:01:08 +0100

    Dostosowanie instalacji do wymaganych bibliotek w projekcie

--- instalacja_bibliotek.bat
+++ instalacja_bibliotek.bat
@@ -1,6 +1,6 @@
 @echo off
 
-SET OSGEO4W_ROOT=C:\Program Files\QGIS 3.40.13
+SET OSGEO4W_ROOT=C:\Program Files\QGIS 3.40.3
 
 CALL "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 
@@ -8,7 +8,7 @@ CALL "%OSGEO4W_ROOT%\bin\o4w_env.bat"
 echo.
 echo Instalacja wymaganych bibliotek Python dla GISMOOTH...
 echo.
-python -m pip install psycopg2-binary sqlalchemy pandas matplotlib seaborn openpyxl fiona geopandas rasterio pyproj shapely open3d laspy numpy
+python -m pip install psycopg2-binary sqlalchemy pandas matplotlib seaborn openpyxl fiona geopandas rasterio pyproj shapely open3d laspy numpy folium mapclassify 
 
 echo.
 echo Instalacja zakonczona.
